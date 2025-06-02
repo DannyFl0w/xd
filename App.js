40320a562af5f5eacbdc048b0ca8bc324b652d2e
DannyFl0w <a1124250012@utch.edu.mx>
2025-06-02 07:30:45 -0700

    comins

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+
+var array = [1, 2, 3, 4, 5];
+var jsonX = [
+  { "name": "John", "age": 30 },
+  { "name": "Jane", "age": 25 },
+  { "name": "Doe", "age": 22 }
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
+
+      <FlatList
+        data={jsonX}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => (
+            <Text>{item.name},{item.age}</Text>
+        )}
+      /> 
+
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
